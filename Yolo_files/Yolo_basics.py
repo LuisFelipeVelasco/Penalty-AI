7974e7915afac426ba11ca972656033acf5d904b
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-03-28 13:08:29 -0500

    improve conditional of evaluate_change

--- Yolo_files/Yolo_basics.py
+++ Yolo_files/Yolo_basics.py
@@ -8,12 +8,14 @@ def load_model(model_path):
     model=YOLO(model_path)
     return model
 
-#evaluate_change look if the coordinates of the object change , in that case returns true
+#evaluate_change look if the coordinates of the object change more than 5 , in that case returns true
 def evaluate_change(x1,x2,y1,y2,last_coordinates):
     last_x1, last_y1, last_x2, last_y2 = last_coordinates
-    change_x = abs(x1 - last_x1) +  + abs(x2 - last_x2)
-    change_y= abs(y1 - last_y1) +  + abs(y2 - last_y2) 
-    if change_x>20 or change_y>20:
+    change_x1 = abs(x1 - last_x1)
+    change_x2 = abs(x2 - last_x2)
+    change_y1= abs(y1 - last_y1)
+    change_y2=abs(y2 - last_y2) 
+    if (change_x1>5 and change_x2 >5) or (change_y1>5 and change_y2 >5) :
         return True
     else:
         return False
@@ -25,8 +27,7 @@ model=load_model("yolov8n.pt")
 results=model("Images/Penalty_1.png")
 #Inserts the bounding boxes and labels into the image
 img = results[0].plot()
-cv.imshow("Image", img)
-cv.waitKey(0)
+
 capture=cv.VideoCapture("Videos/06.mp4")
 last_coordinates=[]
 global is_significant_change 
@@ -38,6 +39,7 @@ while True:
         for box in results[0].boxes: #boxes is used to get the bounding box coordinates
             if box.cls==32: #If the class of the box is 32 (which corresponds to a soccer ball in the COCO dataset)
                 x1,y1,x2,y2=box.xyxy[0].tolist() #We use list unpacking to get the coordinates of the bounding box of the soccer ball
+                print(x1,y1,x2,y2)
                 if last_coordinates: 
                     is_significant_change = evaluate_change(x1, x2, y1, y2, last_coordinates) 
                 last_coordinates=[x1,y1,x2,y2]
